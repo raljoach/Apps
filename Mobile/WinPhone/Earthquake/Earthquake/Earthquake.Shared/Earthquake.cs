36f47dc21b38e296979d55b4e5dedeee1807e493
Ralph Joachim <r19serious@yahoo.com>
2014-11-05 00:06:24 -0800

    Earthquake Phone app initial

---
+++ Mobile/WinPhone/Earthquake/Earthquake/Earthquake.Shared/Earthquake.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Earthquake
+{
+    public class Earthquake
+    {
+        public string eqid { get; set; }
+        public double magnitude { get; set; }
+        public double lng { get; set; }
+        public string src { get; set; }
+        public string datetime { get; set; }
+        public double depth { get; set; }
+        public double lat { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public List<Earthquake> earthquakes { get; set; }
+    }
+}
---
+++ Mobile/WinPhone/Earthquake/Earthquake/Earthquake.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Earthquake
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            fetch();
+        }
+
+        private async void fetch()
+        {
+            HttpClient client = new HttpClient();
+            
+             var response = await client.GetStreamAsync(new Uri(
+                "http://api.geonames.org/earthquakesJSON?formatted=true&north=44.1&south=-9.9&east=-22.4&west=55.2&username=mkoppelman"));
+
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(RootObject));
+            RootObject feed = (RootObject)ser.ReadObject(response);
+            System.Diagnostics.Debug.WriteLine(feed.earthquakes[0].src + " " + feed.earthquakes[0].magnitude);
+
+        }
+    }
+}
