61b3a690bcaaf3f16b0b98d9be2fc9feac00fe5a
ralph.joachim <ralph.joachim@docusign.com>
2016-05-11 01:57:52 -0700

    new apps

---
+++ KazmonWatcher/Program.cs
@@ -0,0 +1,111 @@
+﻿using Common;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KazmonWatcher
+{
+    public class Config
+    {
+        public List<string> Environments { get; set; }
+        public List<string> Counters { get; set; }
+
+        public Creds Creds { get; set; }
+    }
+    public class Creds
+    {
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+    public class Program
+    {
+        private static string kazmonUrl = "https://kazmon.docusign.net:17005/Dashboard/";
+        public static void Main(string[] args)
+        {
+            var file = "config.json";
+            var json = FileIO.ReadAll(file);
+            var config = JsonConvert.DeserializeObject<Config>(json);
+            var driver = new ChromeDriver();//PhantomJSExt.InitPhantomJS();
+            //var driver = new RemoteWebDriver(new Uri(kazmonUrl), DesiredCapabilities.Chrome());
+            try
+            {
+                driver.Navigate().GoToUrl(kazmonUrl);
+                EnterCreds(driver, config);
+                Console.WriteLine("Please enter google authenticator code into browser. Then hit enter in console window.");//id='softTokenAttempt.passcode'
+                Console.ReadKey();
+
+                SetEnvironments(config, driver);
+                SetCounters(config, driver);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        private static void EnterCreds(IWebDriver driver, Config config)
+        {
+            var userId = "cred_userid_inputtext";
+            var passId = "cred_password_inputtext";
+            var userBox = driver.FindElement(By.Id(userId));
+            userBox.SendKeys(config.Creds.UserName);
+            
+            
+            var passBox = driver.FindElement(By.Id(passId));
+            passBox.Click();//passBox.SendKeys(config.Creds.Password);
+            /*
+            var btn = driver.FindElement(By.Id("cred_sign_in_button"));
+            btn.Click();*/
+        }
+
+        private static void SetCounters(Config config, IWebDriver driver)
+        {
+            var section = driver.FindElement(By.Id("metricCounters"));
+            SetCheckBoxes(config, driver, section, config.Counters);
+        }
+
+        private static void SetEnvironments(Config config, IWebDriver driver)
+        {
+            var section = driver.FindElement(By.Id("metricEnvironments"));
+            SetCheckBoxes(config, driver, section, config.Environments);
+        }
+
+        private static void SetCheckBoxes(Config config, IWebDriver driver, IWebElement section, List<string> selections)
+        {
+
+            var choices = section.FindElements(By.XPath("//input[@type='checkbox']"));
+            //var listItems = section.FindElements(By.TagName("li"));
+            //foreach (var li in listItems)
+            //{
+                //var c = li.FindElement(By.XPath("//input[@type='checkbox']"));
+                foreach (var c in choices)
+                {
+                //var envOp = c.Text;
+                if (!c.Displayed) { continue; }
+                var envOp = c.GetAttribute("value");
+                    if (selections.Contains(envOp, StringComparer.CurrentCulture))
+                    {
+                        //c.aa
+                        //setAttribute(driver, c, "checked", "true");
+                        if (!c.Selected)
+                        {
+                            c.Click();
+                        }
+                    }
+                //}
+            }
+        }
+
+        //private static void setAttribute(RemoteWebDriver driver, IWebElement element, String attName, String attValue)
+        //{
+        //    driver.ExecuteScript("arguments[0].setAttribute(arguments[1], arguments[2]);",
+        //            element, attName, attValue);
+        //}
+    }
+}
---
+++ RandomQuestionAsker/Program.cs
@@ -0,0 +1,234 @@
+﻿using Common;
+using InterviewsLib;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RandomQuestionAsker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var topics = new List<string>();
+            var companies = new List<string>();
+            var keywords = new List<string>();
+            var tags = new List<string>();
+            if(args!=null && args.Length>0)
+            {
+                for(int j=0; j<args.Length; j++)
+                {
+                    var flag = args[j].ToLower();
+                    if(flag.StartsWith("-"))
+                    {
+                        flag = flag.Substring(1);
+                    }
+                    switch(flag)
+                    {
+                        case "topic":
+                            ++j;
+                            GetParamValues(args, ref j, topics);
+                            break;
+                        case "company":
+                            ++j;
+                            GetParamValues(args, ref j, companies);
+                            break;
+                        case "keyword":
+                            ++j;
+                            GetParamValues(args, ref j, keywords);
+                            break;
+                        case "tag":
+                            ++j;
+                            GetParamValues(args, ref j, tags);
+                            break;
+                        default:
+                            throw new InvalidOperationException(string.Format("Error: Unhandled parameter flag '{0}", flag));
+                    }
+                }
+            }
+            var file = "questions.json";
+            var index = BuildIndexes(file);
+            AskQuestions(index, topics, companies, keywords, tags);
+        }
+
+        private static void GetParamValues(string[] args, ref int j, List<string> list)
+        {
+            for (; j < args.Length; j++)
+            {
+                var nextArg = args[j];
+                if (nextArg.StartsWith("-")) { --j; break; }
+                list.Add(nextArg);
+            }
+        }
+
+        public enum IndexType
+        {
+            Topic,
+            Company,
+            Tag,
+            Keyword
+        }
+
+        private static void AskQuestions(Index index, List<string> topics, List<string> companies, List<string> keywords, List<string> tags)
+        {
+            int numIndexes = 0;
+            var indexMap = new Dictionary<IndexType, Dictionary<string, List<Question>>>();
+            bool noRestrictions = topics.Count==0 && companies.Count==0 && keywords.Count==0 && tags.Count==0;
+
+            if (index.TopicIndex.Count > 0)
+            {
+                if (noRestrictions || topics.Count > 0)
+                {
+                    ++numIndexes;
+                    indexMap.Add(IndexType.Topic,index.TopicIndex);
+                }
+            }
+            if (index.KeywordIndex.Count > 0)
+            {
+                if (noRestrictions || keywords.Count > 0)
+                {
+                    ++numIndexes;
+                    indexMap.Add(IndexType.Keyword, index.KeywordIndex);
+                }
+            }
+            if (index.CompanyIndex.Count > 0)
+            {
+                if (noRestrictions || companies.Count > 0)
+                {
+                    ++numIndexes;
+                    indexMap.Add(IndexType.Company, index.CompanyIndex);
+                }
+            }
+            if (index.TagIndex.Count > 0)
+            {
+                if (noRestrictions || tags.Count > 0)
+                {
+                    ++numIndexes;
+                    indexMap.Add(IndexType.Tag, index.TagIndex);
+                }
+            }
+            int minsToAnswer = 5;
+            var minsToAnswerTS = TimeSpan.FromMinutes(minsToAnswer);
+            int seed = (int)DateTime.Now.Ticks & 0x0000FFFF;
+            var random = new Random(seed);
+            Logger.Debug("Random seed: {0}", seed);
+            if (numIndexes > 0)
+            {
+                var choice = random.Next(numIndexes);
+                var iType = indexMap.Keys.ToArray()[choice];
+                var cIndex = indexMap[iType];
+
+                Question q;
+                if (noRestrictions)
+                {
+                    var numK = cIndex.Keys.Count;
+                    choice = random.Next(numK);
+                    var k = cIndex.Keys.ToArray()[choice];
+
+                    var qList = cIndex[k];
+                    var numQ = qList.Count;
+                    choice = random.Next(numQ);
+                    q = qList[choice];
+                }
+                else
+                {
+                    switch(iType)
+                    {
+                        case IndexType.Company:
+                            break;
+                        default:
+                            throw new InvalidOperationException(string.Format("Error: Unhandled index type: {0}", iType));
+                    }
+                }
+
+                while (true)
+                {
+                    Logger.Debug("Ready for a question? Hit enter to continue or q to quit.");
+                    var key = Console.ReadKey().KeyChar;
+                    if (key == 'q' || key == 'Q') { break; }
+                    Logger.Border();
+                    Logger.Debug("Here's your question:");
+                    Logger.Border();
+                    Logger.Debug(q.Text);
+                    Logger.Border();
+
+                    var timeout = DateTime.Now.Add(minsToAnswerTS);
+                    var halfSecCount = 0;
+                    while (DateTime.Now <= timeout)
+                    {
+                        Thread.Sleep(TimeSpan.FromMilliseconds(500));
+                        ++halfSecCount;
+                        if (halfSecCount % 2 == 0)
+                        {
+                            minsToAnswerTS.Subtract(TimeSpan.FromSeconds(1));
+                        }
+                        Logger.Debug("Time remaining: {0}:{1}", minsToAnswerTS.Minutes, minsToAnswerTS.Seconds);
+                    }
+
+                    Logger.Debug("Times up!");
+                    //Buzzer sounds!   
+                }
+            }
+        }
+
+        private static Index BuildIndexes(string file)
+        {
+            var indexFile = "index.json";
+            Index index;
+            if (File.Exists(indexFile))
+            {
+                index = JsonConvert.DeserializeObject<Index>(FileIO.ReadAll(indexFile));
+            }
+            else
+            {
+                var interviewData = JsonConvert.DeserializeObject<InterviewData>(FileIO.ReadAll(file));
+                var keywordIndex = new Dictionary<string, List<Question>>(StringComparer.CurrentCulture);
+                var companyIndex = new Dictionary<string, List<Question>>(StringComparer.CurrentCulture);
+                var topicIndex = new Dictionary<string, List<Question>>(StringComparer.CurrentCulture);
+                foreach (var q in interviewData.Questions)
+                {
+                    AddToIndex(keywordIndex, q.Keywords, q);
+                    AddToIndex(companyIndex, q.Company, q);
+                    AddToIndex(topicIndex, q.Topic, q);
+                }
+                index = new Index();
+                index.TopicIndex = topicIndex;
+                index.CompanyIndex = companyIndex;
+                index.KeywordIndex = keywordIndex;
+                var json = JsonConvert.SerializeObject(index);
+
+                FileIO.Write(indexFile, json);
+            }
+            return index;
+        }
+
+        private static void AddToIndex(Dictionary<string, List<Question>> index, string key, Question q)
+        {
+            if (key == null) return;
+            List<Question> list;
+            if(index.ContainsKey(key))
+            {
+                list = index[key];
+            }
+            else
+            {
+                list = new List<Question>();
+                index.Add(key, list);
+            }
+            list.Add(q);            
+        }
+
+        private static void AddToIndex(Dictionary<string, List<Question>> index, List<string> keys, Question q)
+        {
+            foreach(var k in keys)
+            {
+                AddToIndex(index, k, q);
+            }
+        }
+    }
+}
