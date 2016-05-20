f3ec33d98aab88edcc2029fedd7e717f613eb163
ralph.joachim <ralph.joachim@docusign.com>
2016-05-20 00:27:36 -0700

    minor updates

--- HouseFlipper/Program.cs
+++ HouseFlipper/Program.cs
@@ -16,79 +16,32 @@ namespace HouseFlipper
         private static string dataRelFolder = @"data";
         static void Main(string[] args)
         {
-            string dataFolder = GetDataFolder();
+            string dataFolder = null;
+            if (args != null && args.Length > 0)
+            {
+                dataFolder = args[0];
+                if (!Directory.Exists(dataFolder))
+                {
+                    Logger.Debug("Missing data folder: {0}", dataFolder);
+                    return;
+                }
+            }
+            else
+            {
+                dataFolder = GetDefaultFolder();
+            }
+
+            Logger.Debug("Using data folder: {0}", dataFolder);
             var subDirs = Directory.GetDirectories(dataFolder);
             var files = Directory.GetFiles(dataFolder, "*.csv", SearchOption.AllDirectories);
+            if (files == null || files.Count() == 0)
+            {
+                Logger.Debug("No files in data folder");
+            }
             List<MlsRow> mlsRows = new List<MlsRow>();
             var soldHash = new Dictionary<string, MlsRow>();
             var flippedHash = new Dictionary<string, Dictionary<string, List<MlsRow>>>();
-            foreach (var f in files)
-            {
-                bool isFirstLine = true;
-
-                string[] colNames = null;
-                var rowNum = 0;
-                foreach (var line in FileIO.ReadFrom(f))
-                {
-                    ++rowNum;
-                    var cols = line.Split(new string[] { "\",\"" }, StringSplitOptions.None);
-                    if (isFirstLine)
-                    {
-                        isFirstLine = false;                        
-                        var data = new StringDictionary();
-                        colNames = new string[cols.Length];
-                        for (var j = 0; j < cols.Length; j++)
-                        {
-                            var field = cols[j].Replace("\"", string.Empty);
-                            colNames[j] = field;
-                        }                                             
-                    }
-                    else
-                    {
-                        var data = new StringDictionary();
-                        for (var j = 0; j < cols.Length; j++)
-                        {
-                            var field = cols[j].Replace("\"",string.Empty);
-                            data.Add(colNames[j], field);
-                        }
-                        var mlsRow = new MlsRow(data);
-                        mlsRows.Add(mlsRow);
-                        if (mlsRow.StatusValue == MlsStatus.Sold)
-                        {
-                            //var houseID = mlsRow.MLNumber.Trim().ToLower();
-                            var temp = mlsRow.Address.Trim() + mlsRow.City.Trim() + mlsRow.PostalCode.Trim();
-                            var houseID = temp.ToLower();
-                            if (soldHash.ContainsKey(houseID))
-                            {
-                                var zip = mlsRow.PostalCode;
-                                Dictionary<string, List<MlsRow>> zipHash;
-                                if(flippedHash.ContainsKey(zip))
-                                {
-                                    zipHash = flippedHash[zip];
-                                }
-                                else
-                                {
-                                    zipHash = new Dictionary<string, List<MlsRow>>();
-                                    flippedHash.Add(zip, zipHash);
-                                }
-                                if(zipHash.ContainsKey(houseID))
-                                {
-                                    var soldRecords = zipHash[houseID];
-                                    soldRecords.Add(mlsRow);
-                                }
-                                else
-                                {
-                                    zipHash.Add(houseID, new List<MlsRow>() { soldHash[houseID], mlsRow });
-                                }
-                            }
-                            else
-                            {
-                                soldHash.Add(houseID, mlsRow);
-                            }                            
-                        }
-                    }
-                }
-            }
+            ReadFiles(files, mlsRows, soldHash, flippedHash);
             var mlsData = new MlsData() { Data = mlsRows };
             var json = JsonConvert.SerializeObject(mlsData);
             FileIO.Write("mls.json", json);
@@ -97,14 +50,14 @@ namespace HouseFlipper
             Console.WriteLine("Number of zipcodes with flipped houses: {0}", flippedHash.Keys.Count);
             Logger.Border();
             var zipCount = 0;
-            foreach(var zip in flippedHash.Keys)
+            foreach (var zip in flippedHash.Keys)
             {
-                ++zipCount;                
+                ++zipCount;
                 var preFlip = new HouseCharacteristics();
                 var postFlip = new HouseCharacteristics();
                 var zipHash = flippedHash[zip];
                 double numHouses = zipHash.Keys.Count;
-             
+
                 Console.WriteLine("#{0} Zip code: {1} => {2} flipped houses", zipCount, zip, numHouses);
                 var count = 0;
                 foreach (var houseId in zipHash.Keys)
@@ -115,10 +68,11 @@ namespace HouseFlipper
                     soldRecords.Sort();
                     var lastSold = soldRecords[soldRecords.Count - 1];
                     MlsRow firstSold = null;
-                    for(var k=soldRecords.Count-2; k>=0; k--)
+                    for (var k = soldRecords.Count - 2; k >= 0; k--)
                     {
                         var prevSold = soldRecords[k];
-                        if (lastSold.CloseDateValue() < prevSold.CloseDateValue()) {
+                        if (lastSold.CloseDateValue() < prevSold.CloseDateValue())
+                        {
                             throw new InvalidOperationException();
                         }
                         var time = lastSold.CloseDateValue().Subtract(prevSold.CloseDateValue());
@@ -139,12 +93,12 @@ namespace HouseFlipper
                     postFlip.Price += lastPrice;
                     var profit = lastPrice - firstPrice;
                     Console.WriteLine("{0}) {1}: {2} in zip {3} sold for {4} in {5}, sold again for {6} in {7}, Profit: ${8}", count, firstSold.MLNumber, homeAddress, zip, firstSold.CurrentPrice, firstSold.CloseDate, lastSold.CurrentPrice, lastSold.CloseDate, ToString(profit));
-                    
+
                 }
 
                 preFlip.Price = preFlip.Price / numHouses;
                 postFlip.Price = postFlip.Price / numHouses;
-                results.Add(zip, new FlippedHouse(zip,preFlip,postFlip)/* { PreFlip = preFlip, PostFlip = postFlip }*/);
+                results.Add(zip, new FlippedHouse(zip, preFlip, postFlip)/* { PreFlip = preFlip, PostFlip = postFlip }*/);
 
                 Logger.Border();
                 Console.WriteLine("Average price prior to flip: ${0}", ToString(preFlip.Price));
@@ -156,31 +110,110 @@ namespace HouseFlipper
             list.Sort();
             Console.WriteLine("Zipcodes ordered by profitability:");
             Logger.Border();
-            for(int k=0; k<list.Count; k++)
+            for (int k = 0; k < list.Count; k++)
             {
                 var r = list[k];
-                Console.WriteLine("{0}) {1} | ${2}", k+1, r.Zipcode, ToString(r.Profit));
+                Console.WriteLine("{0}) {1} | ${2}", k + 1, r.Zipcode, ToString(r.Profit));
             }
             Logger.Border();
             Console.WriteLine();
-            Console.WriteLine("Program has ended. Hit any key to exit.");
-            Console.ReadKey();
+            //Console.WriteLine("Program has ended. Hit any key to exit.");
+            //Console.ReadKey();
+        }
+
+        private static void ReadFiles(string[] files, List<MlsRow> mlsRows, Dictionary<string, MlsRow> soldHash, Dictionary<string, Dictionary<string, List<MlsRow>>> flippedHash)
+        {
+            foreach (var f in files)
+            {
+                bool isFirstLine = true;
+
+                string[] colNames = null;
+                var rowNum = 0;
+                foreach (var line in FileIO.ReadFrom(f))
+                {
+                    ++rowNum;
+                    var cols = line.Split(new string[] { "\",\"" }, StringSplitOptions.None);
+                    if (isFirstLine)
+                    {
+                        isFirstLine = false;
+                        var data = new StringDictionary();
+                        colNames = new string[cols.Length];
+                        for (var j = 0; j < cols.Length; j++)
+                        {
+                            var field = cols[j].Replace("\"", string.Empty);
+                            colNames[j] = field;
+                        }
+                    }
+                    else
+                    {
+                        var data = new StringDictionary();
+                        for (var j = 0; j < cols.Length; j++)
+                        {
+                            var field = cols[j].Replace("\"", string.Empty);
+                            data.Add(colNames[j], field);
+                        }
+                        var mlsRow = new MlsRow(data);
+                        mlsRows.Add(mlsRow);
+                        if (mlsRow.StatusValue == MlsStatus.Sold)
+                        {
+                            //var houseID = mlsRow.MLNumber.Trim().ToLower();
+                            var temp = mlsRow.Address.Trim() + mlsRow.City.Trim() + mlsRow.PostalCode.Trim();
+                            var houseID = temp.ToLower();
+                            if (soldHash.ContainsKey(houseID))
+                            {
+                                var zip = mlsRow.PostalCode;
+                                Dictionary<string, List<MlsRow>> zipHash;
+                                if (flippedHash.ContainsKey(zip))
+                                {
+                                    zipHash = flippedHash[zip];
+                                }
+                                else
+                                {
+                                    zipHash = new Dictionary<string, List<MlsRow>>();
+                                    flippedHash.Add(zip, zipHash);
+                                }
+                                if (zipHash.ContainsKey(houseID))
+                                {
+                                    var soldRecords = zipHash[houseID];
+                                    soldRecords.Add(mlsRow);
+                                }
+                                else
+                                {
+                                    zipHash.Add(houseID, new List<MlsRow>() { soldHash[houseID], mlsRow });
+                                }
+                            }
+                            else
+                            {
+                                soldHash.Add(houseID, mlsRow);
+                            }
+                        }
+                    }
+                }
+                if (rowNum == 0)
+                {
+                    Logger.Debug("Empty file: '{0}'", f);
+                }
+                else if (rowNum == 1)
+                {
+                    Logger.Debug("Header row found, but no data rows exist in file:'{0}'", f);
+                }
+            }
         }
 
         private static string ToString(double num)
-        {           
+        {
             var str = num.ToString();
             var index = str.IndexOf('.');
             var f = "";
-            if(index>=0)
+            if (index >= 0)
             {
                 f = str.Substring(index);
                 str = str.Substring(0, index);
             }
             var k = 0;
-            for(int j=str.Length-1; j>=0; j--)
-            {              
-                if(k>0 && k%3==0)
+            for (int j = str.Length - 1; j >= 0; j--)
+            {
+                if (k > 0 && k % 3 == 0)
                 {
                     f = "," + f;
                 }
@@ -219,7 +252,7 @@ namespace HouseFlipper
             public int CompareTo(object obj)
             {
                 var other = obj as FlippedHouse;
-                if(other==null) { return 1; }
+                if (other == null) { return 1; }
                 return this.Profit.CompareTo(other.Profit);
             }
         }
@@ -230,13 +263,13 @@ namespace HouseFlipper
             public double Price { get; set; }
         }
 
-        private static string GetDataFolder()
+        private static string GetDefaultFolder()
         {
-            var exeLoc = Environment.CurrentDirectory;
+            var exeLoc = typeof(Program).Assembly.Location;
             var projDir = exeLoc;
             while (!projDir.EndsWith("bin"))
             {
-                projDir = Path.GetDirectoryName(exeLoc);
+                projDir = Path.GetDirectoryName(projDir);
             }
             projDir = Path.GetDirectoryName(projDir);
             var dataFolder = Path.Combine(projDir, dataRelFolder);
